b38b0afb23b884c9a3e9f7d184fafb4d77019f75
Talon <ak1998@live.nl>
2026-06-28 21:49:51 +0200

    Added spring runner

--- src/main/java/discord/ibaraki/Ibaraki.java
+++ src/main/java/discord/ibaraki/Ibaraki.java
@@ -4,11 +4,13 @@ import discord.ibaraki.listeners.MessageListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ibaraki {
     static void main(String[] args) {
+        SpringApplication.run(Ibaraki.class, args);
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("DISCORD_TOKEN") != null ? dotenv.get("DISCORD_TOKEN") : System.getenv("DISCORD_TOKEN");
 
