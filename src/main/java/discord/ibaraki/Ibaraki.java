281b0b94ec9133ea1b4e06c2b2dd7611a44e0379
Talon <ak1998@live.nl>
2026-06-28 21:41:17 +0200

    Changed to spring boot

--- src/main/java/discord/ibaraki/Ibaraki.java
+++ src/main/java/discord/ibaraki/Ibaraki.java
@@ -4,7 +4,9 @@ import discord.ibaraki.listeners.MessageListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Ibaraki {
     static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
