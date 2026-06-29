4edb475e4b527d25c62459c11e4799c7667ccf0c
Talon <ak1998@live.nl>
2026-06-29 11:09:32 +0200

    Changed messageLIstener class

--- src/main/java/discord/ibaraki/listeners/MessageListener.java
+++ src/main/java/discord/ibaraki/listeners/MessageListener.java
@@ -1,12 +1,9 @@
 package discord.ibaraki.listeners;
 
-import discord.ibaraki.helpers.JsonHelper;
 import discord.ibaraki.records.Command;
-import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,15 +12,6 @@ public class MessageListener extends ListenerAdapter {
     private final Map<String, Command> commandCache = new HashMap<>();
 
     public MessageListener() {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        String jsonPath = dotenv.get("COMMAND_LISTENER_PATH") != null ?
-                dotenv.get("COMMAND_LISTENER_PATH")
-                : System.getenv("COMMAND_LISTENER_PATH");
-
-        if (jsonPath == null || jsonPath.isEmpty()) {
-            System.err.println("WARNING: COMMAND_LISTENER_PATH environment variable is missing!");
-            return;
-        }
     }
 
     @Override
