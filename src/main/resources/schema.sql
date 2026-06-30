fa6d7da1f631e4360abd7abb959ecf87619d73e2
Talon <ak1998@live.nl>
2026-06-30 23:36:13 +0200

    Updated schema to migrate tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,67 +1,55 @@
-DROP TABLE IF EXISTS embed;
-DROP TABLE IF EXISTS command_events;
-DROP TABLE IF EXISTS command_responses;
-DROP TABLE IF EXISTS commands;
-DROP TABLE IF EXISTS command_types;
-DROP TABLE IF EXISTS permissions;
-DROP TABLE IF EXISTS server_mods;
-DROP TABLE IF EXISTS servers;
+CREATE TABLE IF NOT EXISTS servers (
+    id bigserial PRIMARY KEY,
+    name varchar(60) NOT NULL
+);
 
-CREATE TABLE users (
-    id BB
-)
+CREATE TABLE IF NOT EXISTS server_mods (
+    id bigserial PRIMARY KEY,
+    name varchar(60) NOT NULL,
+    permissions jsonb NOT NULL
+);
 
-CREATE TABLE servers (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS permissions (
+    id bigserial PRIMARY KEY,
+    name varchar(15) NOT NULL
+);
 
-CREATE TABLE server_mods(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    permissions JSONB NOT NULL
-)
+CREATE TABLE IF NOT EXISTS command_types (
+    id bigserial PRIMARY KEY,
+    name varchar(60) NOT NULL
+);
 
-CREATE TABLE permissions(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(15) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS commands (
+    id bigserial PRIMARY KEY,
+    name varchar(60) NOT NULL,
+    server_id bigint NOT NULL,
+    command_type_id bigint NOT NULL,
+    FOREIGN KEY (server_id) REFERENCES servers (id) ON DELETE CASCADE,
+    FOREIGN KEY (command_type_id) REFERENCES command_types (id) ON DELETE CASCADE
+);
 
-CREATE TABLE commands(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    server_id BIGINT NOT NULL,
-    command_type_id BIGINT NOT NULL,
-    FOREIGN KEY (server_id) REFERENCES servers(id) ON DELETE CASCADE
-    FOREIGN KEY (command_type_id) REFERENCES command_types(id) ON DELETE CASCADE
-)
+CREATE TABLE IF NOT EXISTS command_responses (
+    id bigserial PRIMARY KEY,
+    command_id bigint NOT NULL,
+    text text NOT NULL,
+    FOREIGN KEY (command_id) REFERENCES commands (id) ON DELETE CASCADE
+);
 
-CREATE TABLE command_types(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS command_events (
+    command_id bigint PRIMARY KEY,
+    on_repeat boolean NOT NULL,
+    repeat_interval_seconds int,
+    FOREIGN KEY (command_id) REFERENCES commands (id) ON DELETE CASCADE
+);
 
-CREATE TABLE command_responses(
-    id BIGSERIAL PRIMARY KEY,
-    text TEXT NOT NULL,
-    FOREIGN KEY (command_id) REFERENCES commands(id) ON DELETE CASCADE
-)
-
-CREATE TABLE command_events(
-    command_id BIGSERIAL PRIMARY KEY,
-    on_repeat BOOLEAN NOT NULL,
-    repeat_interval_seconds INT,
-    FOREIGN KEY (command_id) REFERENCES commands(id) ON DELETE CASCADE
-)
-
-CREATE TABLE embed(
-    id BIGSERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS embed (
+    id bigserial PRIMARY KEY,
     type VARCHAR(60),
-    header VARCHAR(60),
-    content VARCHAR(120),
-    footer VARCHAR(120),
-    image_url VARCHAR(500),
-    command_id BIGINT NOT NULL UNIQUE,
-    FOREIGN KEY (command_id) REFERENCES commands(id) ON DELETE CASCADE
-)
+    header varchar(60),
+    content varchar(120),
+    footer varchar(120),
+    image_url varchar(500),
+    command_id bigint NOT NULL UNIQUE,
+    FOREIGN KEY (command_id) REFERENCES commands (id) ON DELETE CASCADE
+);
 
