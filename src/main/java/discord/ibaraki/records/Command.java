f7943d12975fb544ed3ef7b40d74f8b28658ff2e
Talon <ak1998@live.nl>
2026-07-01 00:23:17 +0200

    Match database table "commands"

--- src/main/java/discord/ibaraki/records/Command.java
+++ src/main/java/discord/ibaraki/records/Command.java
@@ -1,7 +1,13 @@
 package discord.ibaraki.records;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("commands")
-public record Command (@Id Long id, String name, String response, String emote) { }
+public record Command (
+        @Id Long id,
+        String name,
+        @Column("server_id") Long serverId,
+        @Column("command_type_id") Long commandTypeId
+) { }
